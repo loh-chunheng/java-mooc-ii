d8a6b11a969074084343e2433fcbcc0d9d17a39c
loh-chunheng <chunheng.loh@gmail.com>
2026-01-12 21:21:32 +0800

    Finish Part 8-1 and 8-2

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,22 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("end")) {
+                break;
+            }
+            
+            try {
+                int number = Integer.valueOf(input);
+                System.out.println(number * number * number);
+            } catch (Exception e) {
+                System.out.println("Not a valid number!");
+            }
+            
+        }
 
     }
 }
--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,26 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int countPositive = 0;
+        
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            if (number > 0) {
+                sum += number;
+                countPositive ++;
+            }
+        }
+        
+        if (countPositive > 0) {
+            System.out.println(sum * 1.0 / countPositive);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
     }
 }
