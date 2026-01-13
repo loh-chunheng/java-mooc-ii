f9625bdca0772b76d8f74024ff870a3ca6c41c00
loh-chunheng <chunheng.loh@gmail.com>
2026-01-13 20:35:48 +0800

    Finish Part 8-05 TodoList

---
+++ part08-Part08_05.TodoList/src/main/java/TodoList.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mulle
+ */
+import java.util.ArrayList;
+
+public class TodoList {
+    private ArrayList<String> list;
+    
+    public TodoList() {
+        this.list = new ArrayList<>();
+    }
+    
+    public void add(String task) {
+        this.list.add(task);
+    }
+    
+    public void print() {
+        int index = 0;
+        
+        while (index < this.list.size()) {
+            System.out.println((index + 1) + ": " + this.list.get(index));
+            index ++;
+        }
+    }
+    
+    public void remove(int number) {
+        this.list.remove(number - 1);
+    }
+}
