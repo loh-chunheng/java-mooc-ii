2694635e6e92c995a54b16bf36eb89e62e054c1f
loh-chunheng <chunheng.loh@gmail.com>
2026-01-13 20:13:06 +0800

    Finish Part 8-04 LiquidContainers2

--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -6,14 +6,40 @@ public class LiquidContainers2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        Container first = new Container();
+        Container second = new Container();
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + first);
+            System.out.println("Second: " + second);
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
+            
+            String[] parts = input.split(" ");
+            int number = Integer.valueOf(parts[1]);
+                        
+            if (parts[0].equals("add")) {
+                first.add(number);
+            }
+            
+            if (parts[0].equals("move")) {
+                
+                if (number > first.contains()) {
+                    number = first.contains();
+                }
+                
+                first.remove(number);
+                second.add(number);
+            }
+
+            if (parts[0].equals("remove")) {
+                second.remove(number);
+            }
+            
+            System.out.println("");
 
         }
     }
