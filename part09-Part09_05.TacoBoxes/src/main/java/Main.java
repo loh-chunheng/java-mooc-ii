a46e576615f981bab3fa9cb5819a0e4034e0647a
loh-chunheng <chunheng.loh@gmail.com>
2026-02-10 21:39:51 +0800

    Completed Part09_05: Implemented interface

--- part09-Part09_05.TacoBoxes/src/main/java/Main.java
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -2,6 +2,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Test your code here!
+        TripleTacoBox threeBox = new TripleTacoBox();
+        CustomTacoBox box = new CustomTacoBox(5);
+        
+        threeBox.eat();
+        System.out.println(threeBox.tacosRemaining());
+        threeBox.eat();
+        threeBox.eat();
+        threeBox.eat();
+        System.out.println(threeBox.tacosRemaining());
+        
+        System.out.println();
+        
+        box.eat();
+        System.out.println(box.tacosRemaining());
+        box.eat();
+        box.eat();
+        box.eat();
+        System.out.println(box.tacosRemaining());
     }
 }
