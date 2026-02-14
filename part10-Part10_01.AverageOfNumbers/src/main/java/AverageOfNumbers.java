09c36e852c52d6f9095d98596be68a8bd35d1bd2
loh-chunheng <chunheng.loh@gmail.com>
2026-02-14 09:54:04 +0800

    Completed Part10_01 and 02: Collection handling with Stream

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,26 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Write your program here
+        ArrayList<String> numbers = new ArrayList<>();
+        
+        System.out.println("Input integers (type \"end\" to stop):");
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equalsIgnoreCase("end")) {
+                break;
+            }
+            
+            numbers.add(input);
+        }
+        
+        double average = numbers.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
+        
+        System.out.println("Average of the numbers: " + average);
 
     }
 }
