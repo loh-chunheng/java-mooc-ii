841eae1110998d64e5ee2915ddee3a43392a8be9
loh-chunheng <chunheng.loh@gmail.com>
2026-02-15 00:22:24 +0800

    Completed Part10_10: Implemented file-to-object pipeline by parsing CSV data into a Stream to instantiate and collect Book objects into an ArrayList

--- part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -3,13 +3,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class ReadingFilesPerLine {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
         String file = "file.txt";
         
         read(file).stream().forEach(s -> System.out.println(s));
@@ -21,7 +21,8 @@ public class ReadingFilesPerLine {
         List<String> rows = new ArrayList<>();
         
         try {
-            Files.lines(Paths.get(file)).forEach(row -> rows.add(row));
+            Files.lines(Paths.get(file))
+                    .forEach(row -> rows.add(row));
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -3,15 +3,38 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // test your method here
+        //Scanner scanner = new Scanner(System.in);
+        
+        String file = "bookfile.txt";
+        
+        readBooks(file).stream()
+                .forEach(book -> 
+                        System.out.printf("%s, %d, %d, %s%n",
+                                book.getName(), book.getPublishingYear(), book.getPagecount(), book.getAuthor()));
 
+    }
+    
+    public static List<Book> readBooks(String file) {
+        
+        List<Book> books = new ArrayList<>();
+        
+        try {
+            books = Files.lines(Paths.get(file))
+                    .map(row -> row.split(","))
+                    .filter(parts -> parts.length >= 4)
+                    .map(parts -> new Book(parts[0],Integer.valueOf(parts[1]),Integer.valueOf(parts[2]),parts[3]))
+                    .collect(Collectors.toCollection(ArrayList::new));
+        } catch (Exception e) {
+            System.out.println("Errors: " + e.getMessage());    
+        }
+        
+        return books;
     }
 
 }
