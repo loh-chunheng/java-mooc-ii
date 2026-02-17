d821adab3c56ce21b1368df5c43e02bd7387524a
loh-chunheng <chunheng.loh@gmail.com>
2026-02-17 18:03:36 +0800

    Completed Part10_15: used regular expressions to check formats of string

--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,39 @@
 
+import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
-        // you can create test code here. Call the methods that you'll implement
-        // during the course of this exercise
+        Scanner scanner = new Scanner(System.in);
+        Checker checker = new Checker();
+        
+        /* System.out.println("Enter a string (day of week):");
+        String word1 = scanner.nextLine();
+        
+        if (checker.isDayOfWeek(word1)) {
+            System.out.println("The form is correct.");
+        } else {
+            System.out.println("The form is incorrect.");
+        }
+        
+        System.out.println("Enter a string (vowels only):");
+        String word2 = scanner.nextLine();
+        
+        if (checker.allVowels(word2)) {
+            System.out.println("The form is correct.");
+        } else {
+            System.out.println("The form is incorrect.");
+        } */
+        
+        System.out.println("Enter time of day in 24-hour format (hh:mm:ss):");
+        String time = scanner.nextLine();
+        
+        if (checker.timeOfDay(time)) {
+            System.out.println("The form is correct.");
+        } else {
+            System.out.println("The form is incorrect.");
+        }
+        
+        
     }
 }
