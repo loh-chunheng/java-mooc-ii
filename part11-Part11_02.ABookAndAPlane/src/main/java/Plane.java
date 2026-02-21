d105960265cec3a50375ecffc6883460eb2cc0ac
loh-chunheng <chunheng.loh@gmail.com>
2026-02-21 22:39:18 +0800

    Completed Part11_02: implemented Book class and Plane class based on given class diagrams

---
+++ part11-Part11_02.ABookAndAPlane/src/main/java/Plane.java
@@ -0,0 +1,12 @@
+
+public class Plane {
+    private String ID;
+    private String model;
+    private int yearOfIntroduction;
+    
+    public Plane(String ID, String model, int yearOfIntroduction) {
+        this.ID = ID;
+        this.model = model;
+        this.yearOfIntroduction = yearOfIntroduction;
+    }
+}
