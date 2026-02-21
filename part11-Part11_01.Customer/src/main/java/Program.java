e62725f3d514ee5d050fc6d00efbbf58a296a2c6
loh-chunheng <chunheng.loh@gmail.com>
2026-02-21 22:33:31 +0800

    Completed Part11_01: implemented Customer class based on given class diagram

--- part11-Part11_01.Customer/src/main/java/Program.java
+++ part11-Part11_01.Customer/src/main/java/Program.java
@@ -1,11 +1,14 @@
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // you can test your class here
+        //Scanner scanner = new Scanner(System.in);
+        
+        Customer customer1 = new Customer("John", "New York", "john_ny@gmail.com");
+        
+        
 
     }
 }
