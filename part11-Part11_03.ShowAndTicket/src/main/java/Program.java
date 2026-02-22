b51e8ed1b461a52e7247abd5d5ef52eb08f9e155
loh-chunheng <chunheng.loh@gmail.com>
2026-02-22 16:04:33 +0800

    Completed Part11_03: Implemented Show and Ticket classes where one Show can sell many tickets

--- part11-Part11_03.ShowAndTicket/src/main/java/Program.java
+++ part11-Part11_03.ShowAndTicket/src/main/java/Program.java
@@ -1,11 +1,18 @@
 
-import java.util.Scanner;
-
+//import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // you can test your classes here
+        //Scanner scanner = new Scanner(System.in);
+        Show avatar = new Show("Avatar", "12:05");
+        Show harryPotter = new Show("Harry Potter", "15:30");
 
+        Ticket ticket1 = new Ticket(1, 1001, avatar);
+        Ticket ticket2 = new Ticket(2, 1002, avatar);
+        Ticket ticket3 = new Ticket(3, 1003, avatar);
+        Ticket ticket4 = new Ticket(11, 2011, harryPotter);
+        Ticket ticket5 = new Ticket(12, 2012, harryPotter);
+        
+        
     }
 }
